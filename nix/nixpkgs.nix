17e0019c5a2fb7a5ffbc0a0aa4909972b72be519
Maciej Kazulak <kazulakm@gmail.com>
2022-05-13 13:39:56 +0200

    Support GHC 9.2

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,6 +1,6 @@
 import (builtins.fetchTarball {
-  name = "nixos-20.09-2021-04-23";
-  url = "https://github.com/NixOS/nixpkgs/archive/4263ba5e133cc3fc699c1152ab5ee46ef668e675.tar.gz";
-  sha256 = "1nzqrdw0lhbldbs9r651zmgqpwhjhh9sssykhcl2155kgsfsrk7i";
+  name = "nixos-21.11-2022-05-13";
+  url = "https://github.com/NixOS/nixpkgs/archive/aa2f845096f72dde4ad0c168eeec387cbd2eae04.tar.gz";
+  sha256 = "0l732ci2g78pcgk9kqn6c18h4j47dhp1dys52cmqhzm4pyi6dl0z";
 })
 
--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-{ nixpkgs ? import ./nix/nixpkgs.nix {}, compiler ? "ghc884" }:
+{ nixpkgs ? import ./nix/nixpkgs.nix {}, compiler ? "ghc921" }:
 
 let
   inherit (nixpkgs) pkgs;
@@ -6,11 +6,20 @@ let
   hlib = pkgs.haskell.lib;
   hpkgs0 = pkgs.haskell.packages.${compiler};
 
+  sources = {
+    cabal-doctest = nixpkgs.fetchFromGitHub {
+      owner = "haskellari";
+      repo = "cabal-doctest";
+    };
+  };
+
   hpkgs = hpkgs0.override {
     overrides = self: super: {
       odpi-libdpi = self.callCabal2nix "odpi-libdpi" ./odpi-libdpi {};
       odpi-simple = self.callCabal2nix "odpi-simple" ./odpi-simple { inherit (self) odpi-libdpi; };
       odpi-streaming = self.callCabal2nix "odpi-streaming" ./odpi-streaming { inherit (self) odpi-simple; };
+
+      cabal-doctest = self.callCabal2nix "cabal-doctest" "${sources.cabal-doctest}" {};
     };
   };
 
@@ -24,6 +33,6 @@ in
       odpic
       cabal-install
       hlint
-      hpkgs0.haskell-language-server
+      #hpkgs0.haskell-language-server
     ];
   }
